f85c7675bcbb2568db796187e3596f4aa25ca78e
AlmirMR <mahmudov.almirr@gmail.com>
2025-11-27 00:48:29 +0300

    этто было быстро

--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -4,8 +4,7 @@ import './StartPage.css';
 const StartPage: React.FC = () => {
   return (
     <div className="start-page">
-      <h1>Рисовалка</h1>
-      <p>Начните рисовать, выбрав инструменты ниже</p>
+      
     </div>
   );
 };
